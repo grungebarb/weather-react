6dfb02af26ebd89de2ff346a7c9eb5afa7676300
grungebarb <barbarallanso@gmail.com>
2021-03-04 09:54:40 +0100

    Added a loader to try and apply changes

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -9,5 +10,15 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
   return (
-    <h2>Hello from Weather</h2>);
+    <div>
+      <h2>Hello from Weather</h2>
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    </div>
+    );
 }
